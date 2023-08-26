63ef81f13cf7d08c14a44b25ac012176c5847492
Denzel Hooke <denzelhooke@hotmail.com>
2023-08-26 00:21:03 -0700

    services component updates

--- urban_siding/urban-siding-frontend/src/app/components/index/services.tsx
+++ urban_siding/urban-siding-frontend/src/app/components/index/services.tsx
@@ -2,5 +2,31 @@ import React from 'react';
 import styles from '@/app/styles/services.module.scss';
 
 export default function Services() {
-  return <div>Services</div>;
+  const services = [
+    {
+      serviceName: 'ROOFING',
+      image: '',
+    },
+    {
+      serviceName: 'SIDING',
+      image: '',
+    },
+    {
+      serviceName: 'FRAMING',
+      image: '',
+    },
+  ];
+  return (
+    <div id={styles.services}>
+      <div className="container">
+        {services.map((service) => {
+          return (
+            <div className={styles.item} style={{ backgroundImage: '' }}>
+              {service.serviceName}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
